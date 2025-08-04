95dcf8896cdcb78a03fd6e1606d2cbd4251d2979
SNMaior <maiordekterov@gmail.com>
2025-08-04 17:16:12 +0500

    Revert "!"
    This reverts commit 72ff0e7a8c3abe2b81f8bbd5b6b207f2c499cbda.

--- trit_cpu_c++.cpp
+++ trit_cpu_c++.cpp
@@ -12,27 +12,27 @@ int main() {
     SetConsoleCP(CP_UTF8);
     std::setlocale(LC_ALL, "");
 
-    // Инициализация процессора
+	// Инициализация процессора
     memory mem(12);
     CPU cpu;
     cpu.attachmemory(&mem);
-
+    
     cpu.registers[0] = tryte(trit::Zero, trit::Plus, trit::Plus);// регистр со значением 4 (0++)
     cpu.registers[1] = tryte(trit::Zero, trit::Plus, trit::Zero); // регистр со значением 3 (0+0)
     cpu.registers[12] = tryte(trit::Plus, trit::Plus, trit::Zero); // регистр со значением 12 (++0)
 
     // пишем команды в память. Сначало счетчик команды, потом команда или аргумент
     mem.set(0, tryte(trit::Plus, trit::Plus, trit::Zero)); // INC
-    mem.set(1, tryte(trit::Zero, trit::Zero, trit::Zero)); // Регистр 0, получаем 5 (+--)
+	mem.set(1, tryte(trit::Zero, trit::Zero, trit::Zero)); // Регистр 0, получаем 5 (+--)
 
     mem.set(2, tryte(trit::Minus, trit::Minus, trit::Zero)); // INC
     mem.set(3, tryte(trit::Zero, trit::Zero, trit::Plus)); // Регистр 1, получаем 2 (0+-)
 
     mem.set(4, tryte(trit::Zero, trit::Plus, trit::Plus)); // ADD
-    mem.set(5, tryte(trit::Zero, trit::Zero, trit::Zero)); // Регистр 0, получаем 7 (+-+)
-    mem.set(6, tryte(trit::Zero, trit::Zero, trit::Plus)); // Регистр 1
+	mem.set(5, tryte(trit::Zero, trit::Zero, trit::Zero)); // Регистр 0, получаем 7 (+-+)
+	mem.set(6, tryte(trit::Zero, trit::Zero, trit::Plus)); // Регистр 1
 
-    mem.set(7, tryte(trit::Zero, trit::Minus, trit::Minus)); // SUB
+	mem.set(7, tryte(trit::Zero, trit::Minus, trit::Minus)); // SUB
     mem.set(8, tryte(trit::Plus, trit::Plus, trit::Zero)); // Регистр 12, получаем 5 (+--)
     mem.set(9, tryte(trit::Zero, trit::Zero, trit::Zero)); // Регистр 0
 
@@ -41,3 +41,4 @@ int main() {
     // запускаем процессор
     cpu.run();
 }
+
